1e6cad0db4f2d2778e12405de6d6a4974ef8518a
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-21 22:53:26 +0700

    Pagination

--- src/main/java/id/anggra/Main.java
+++ src/main/java/id/anggra/Main.java
@@ -11,40 +11,49 @@ public class Main
 {
     public static void main(String[] args)
     {
-        try
+        for (int i = 1; i <= 4; ++i)
         {
-            Document document = Jsoup.connect("https://www.scrapingbee.com/blog")
-                    .timeout(5000)
-                    .get();
-            System.out.println(document.title());
-
-            Element content = document.getElementById("content");
-            System.out.println(content.childrenSize());
-
-            Elements blogs = document.getElementsByClass("p-10");
-            for (Element blog : blogs)
+            try
             {
-                System.out.println(blog.text());
-            }
+                String url = (i==1) ? "https://www.scrapingbee.com/blog/" : "https://www.scrapingbee.com/blog/page/" + i;
 
-            for (Element blog : blogs)
-            {
-                String title = blog.select("h4").text();
-                String link = blog.select("a").attr("href");
+                Document document = Jsoup.connect(url)
+                        .timeout(5000)
+                        .get();
+                System.out.println(document.title() + "/n");
+
+
+                Element content = document.getElementById("content");
+                System.out.println(content.childrenSize());
+                System.out.println();
+
+                Elements blogs = document.getElementsByClass("p-10");
+                for (Element blog : blogs)
+                {
+                    System.out.println(blog.text());
+                    System.out.println();
+                }
+
+                for (Element blog : blogs)
+                {
+                    String title = blog.select("h4").text();
+                    String link = blog.select("a").attr("href");
 //                String headerImage = blog.select("img").first().attr("src");
-                String headerImage = blog.selectFirst("img").attr("src");
-                String authorImage = blog.select("img[src*=authors]").attr("src");
+                    String headerImage = blog.selectFirst("img").attr("src");
+                    String authorImage = blog.select("img[src*=authors]").attr("src");
 
-                System.out.println(title);
-                System.out.println(link);
-                System.out.println(headerImage);
-                System.out.println(authorImage);
+                    System.out.println(title);
+                    System.out.println(link);
+                    System.out.println(headerImage);
+                    System.out.println(authorImage);
+                    System.out.println();
+                }
+
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
             }
-
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
         }
     }
 }
